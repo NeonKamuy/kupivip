ca66f47fa46cc5ea869071dd59a2fb79313da9e1
hfwalter73 <hfwalter73@gmail.com>
2021-03-26 03:45:00 +0400

    Sorting

--- src/views/category/content/index.tsx
+++ src/views/category/content/index.tsx
@@ -1,8 +1,10 @@
 import { wrap } from "lodash";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
+import { ICategoryListItem } from "../../../components/header/categories/interfaces";
 import { ICategoryContent } from "../../product/constants";
 import { useSizes } from "./hooks";
+import { Sort } from "./interfaces";
 
 export const CategoryContent: React.FC<{ contents: ICategoryContent[] }> = (
     props
@@ -26,34 +28,68 @@ export const CategoryContent: React.FC<{ contents: ICategoryContent[] }> = (
         []
     );
 
+    const [sort, setSort] = useState<Sort>(Sort.OurChoice);
+    const handleSortChange = useCallback(
+        (e: React.ChangeEvent<HTMLSelectElement>) => {
+            const target = e.currentTarget;
+            setSort(target.value as Sort);
+        },
+        []
+    );
+
+    const filtered = useMemo(() => {
+        const filtered = checkedSizes.size
+            ? contents.filter((e) =>
+                  e.sizes.some((e) => checkedSizes.has(e.name))
+              )
+            : contents;
+        return [...filtered];
+    }, [checkedSizes, contents]);
+
+    const sorted = useMemo(() => {
+        if (sort === Sort.PriceAsc) return sortByPriceAsc(filtered);
+        if (sort === Sort.PriceDesc) return sortByPriceDesc(filtered);
+
+        return [...filtered];
+    }, [sort, filtered]);
+
     return (
         <div className="category__container">
-            <div className="category__column left">
-                <div className="title">Размер</div>
-                {sizes.map((e) => (
-                    <div>
-                        <input
-                            type="checkbox"
-                            value={e}
-                            id={e}
-                            checked={checkedSizes.has(e)}
-                            onChange={onSizeCheck}
-                        />
-                        <label style={{ marginLeft: 10 }} htmlFor={e}>
-                            {e}
-                        </label>
-                    </div>
-                ))}
+            <div className="select">
+                <select onChange={handleSortChange}>
+                    <option selected disabled>
+                        Сортировать
+                    </option>
+                    <option value={Sort.OurChoice}>Наш выбор</option>
+                    <option value={Sort.PriceAsc}>Цена по возрастанию</option>
+                    <option value={Sort.PriceDesc}>Цена по убыванию</option>
+                </select>
             </div>
 
-            <div className="category__column right">
-                {(checkedSizes.size
-                    ? contents.filter((e) =>
-                          e.sizes.some((e) => checkedSizes.has(e.name))
-                      )
-                    : contents
-                ).map((e) => (
-                    <Link to={`/products/${e.slug}`} className="category__item">
+            <div className="category__columns">
+                <div className="category__column left">
+                    <div className="title">Размер</div>
+                    {sizes.map((e) => (
+                        <div>
+                            <input
+                                type="checkbox"
+                                value={e}
+                                id={e}
+                                checked={checkedSizes.has(e)}
+                                onChange={onSizeCheck}
+                            />
+                            <label style={{ marginLeft: 10 }} htmlFor={e}>
+                                {e}
+                            </label>
+                        </div>
+                    ))}
+                </div>
+                <div className="category__column right">
+                    {sorted.map((e) => (
+                        <Link
+                            to={`/products/${e.slug}`}
+                            className="category__item"
+                        >
                             <div className="category__item__image" />
                             <div>
                                 {e.title} {e.brand}
@@ -69,9 +105,30 @@ export const CategoryContent: React.FC<{ contents: ICategoryContent[] }> = (
                                     {e.price.toLocaleString("ru-RU")} руб.
                                 </span>
                             </div>
-                    </Link>
-                ))}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
 };
+
+function sortByOurChoice(items: ICategoryContent[]): ICategoryContent[] {
+    return items;
+}
+
+function sortByPriceAsc(items: ICategoryContent[]): ICategoryContent[] {
+    return items.sort((a, b) => {
+        if (a.price < b.price) return -1;
+        if (a.price > b.price) return 1;
+        return 0;
+    });
+}
+
+function sortByPriceDesc(items: ICategoryContent[]): ICategoryContent[] {
+    return items.sort((a, b) => {
+        if (a.price > b.price) return -1;
+        if (a.price < b.price) return 1;
+        return 0;
+    });
+}
---
+++ src/views/category/content/interfaces.ts
@@ -0,0 +1,5 @@
+export enum Sort {
+  PriceDesc = "PRICE_DESC",
+  PriceAsc = "Price_ASC",
+  OurChoice = "Our_Choice"
+}
