ff3fa2d662b36f1bf11796b63ff1055f97ec1716
hfwalter73 <hfwalter73@gmail.com>
2021-03-26 02:59:14 +0400

    Category Page Layout

--- src/views/category/content/index.tsx
+++ src/views/category/content/index.tsx
@@ -15,21 +15,23 @@ export const CategoryContent: React.FC<{ contents: ICategoryContent[] }> = (
     );
     const onSizeCheck = useCallback(
         (e: React.ChangeEvent<HTMLInputElement>) => {
-          const target = e.currentTarget;
-          setCheckedSizes(checkedSizes => {
-            const newChecked = new Set([...checkedSizes, target.value]);
-            checkedSizes.has(target.value) && newChecked.delete(target.value);
-            return newChecked;
-          });
+            const target = e.currentTarget;
+            setCheckedSizes((checkedSizes) => {
+                const newChecked = new Set([...checkedSizes, target.value]);
+                checkedSizes.has(target.value) &&
+                    newChecked.delete(target.value);
+                return newChecked;
+            });
         },
         []
     );
 
     return (
-        <div className="container" style={containerStyles}>
-            <div style={sizeStyles}>
+        <div className="category__container">
+            <div className="category__column left">
+                <div className="title">Размер</div>
                 {sizes.map((e) => (
-                    <div style={checkboxStyles}>
+                    <div>
                         <input
                             type="checkbox"
                             value={e}
@@ -37,58 +39,39 @@ export const CategoryContent: React.FC<{ contents: ICategoryContent[] }> = (
                             checked={checkedSizes.has(e)}
                             onChange={onSizeCheck}
                         />
-                        <label style={{marginLeft: 10}} htmlFor={e}>{e}</label>
+                        <label style={{ marginLeft: 10 }} htmlFor={e}>
+                            {e}
+                        </label>
                     </div>
                 ))}
             </div>
 
-            <div style={contentsStyles}>
-                {(checkedSizes.size ? contents.filter(e => e.sizes.some(e => checkedSizes.has(e.name))) : contents).map((e) => (
-                    <Link to={`/products/${e.slug}`}>
-                        <div style={contentStyles}>
-                            <div>Brand: {e.brand}</div>
-                            <div>Title: {e.title}</div>
-                            {e.discountlessPrice && (
-                                <div>
-                                    Discountless Price: {e.discountlessPrice}
-                                </div>
-                            )}
-                            <div>Price: {e.price}</div>
-                        </div>
+            <div className="category__column right">
+                {(checkedSizes.size
+                    ? contents.filter((e) =>
+                          e.sizes.some((e) => checkedSizes.has(e.name))
+                      )
+                    : contents
+                ).map((e) => (
+                    <Link to={`/products/${e.slug}`} className="category__item">
+                            <div className="category__item__image" />
+                            <div>
+                                {e.title} {e.brand}
+                            </div>
+                            <div className="category__item__price__container">
+                                <span className="category__item__price discountless">
+                                    {e.discountlessPrice &&
+                                        e.discountlessPrice.toLocaleString(
+                                            "ru-RU"
+                                        ) + " руб."}
+                                </span>
+                                <span className="category__item__price">
+                                    {e.price.toLocaleString("ru-RU")} руб.
+                                </span>
+                            </div>
                     </Link>
                 ))}
             </div>
         </div>
     );
-};
-
-const containerStyles: any = {
-    color: "black",
-    margin: "50px auto",
-    lineHeight: 1.8,
-};
-
-const contentsStyles: any = {
-    display: "flex",
-    flex: "1",
-    flexWrap: "wrap",
-};
-
-const contentStyles: any = {
-    border: "1px solid black",
-    width: "250px",
-    marginBottom: 50,
-    marginLeft: 20,
-    padding: 20,
-};
-
-const sizeStyles: any = {
-    display: "flex",
-    flexWrap: "wrap",
-    marginBottom: 50,
-    padding: "10px 50px",
-};
-
-const checkboxStyles: any = {
-  width: 150
 };
