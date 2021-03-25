922e9c55dcf37dce5f87d35e7981759b85203b62
hfwalter73 <hfwalter73@gmail.com>
2021-03-26 00:16:22 +0400

    Clickable size cells

--- src/views/product/content/size-picker/index.tsx
+++ src/views/product/content/size-picker/index.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { IProduct } from "../../constants";
 
-export const ProductSizePicker: React.FC<{sizes: IProduct["sizes"]}> = (props)=>{
-  const {sizes} = props;
+export const ProductSizePicker: React.FC<{ sizes: IProduct["sizes"] }> = (
+    props
+) => {
+    const { sizes } = props;
+    const [picked, setPicked] = useState<number | null>(null);
 
-  return (
-    <div className="product__size__picker">
-      <div className="product__size__picker__header">
-        <span>Выберите размер</span>
-        <span className="product__size__picker__header__how_to">Как выбрать</span>
-      </div>
-      <div className="product__size__picker__table">
-        {sizes.map(e => <div className="product__size__picker__table__row"><span>{e.name}</span></div>)}
-      </div>
-    </div>
-  );
-}
+    const togglePicked = useCallback((id: number) => {
+        setPicked((picked) => (picked === id ? null : id));
+    }, []);
+
+    const sizeCells = useMemo(() => {
+        return sizes.map((e) => {
+            let className = "product__size__picker__table__cell";
+            if (e.id === picked) className += " picked";
+
+            return (
+                <div className={className} onClick={() => togglePicked(e.id)}>
+                    <span>{e.name}</span>
+                </div>
+            );
+        });
+    }, [picked]);
+
+    return (
+        <div className="product__size__picker">
+            <div className="product__size__picker__header">
+                <span>Выберите размер</span>
+                <span className="product__size__picker__header__how_to">
+                    Как выбрать
+                </span>
+            </div>
+            <div className="product__size__picker__table">{sizeCells}</div>
+        </div>
+    );
+};
